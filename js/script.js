9cde12e8de051cf74e991a160bfcfd8a1c26a87d
Tymek Pruszkowski <tymoteuszpruszkowski0@gmail.com>
2023-08-28 18:42:24 +0200

    remove tasks

--- js/script.js
+++ js/script.js
@@ -10,6 +10,19 @@
         },
     ]
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    }
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
+
     const render = () => {
         let htmlString = '';
 
@@ -18,22 +31,22 @@
                 <li
                 ${task.done ? " style=\"text-decoration: line-through\"" : ""}
                 >
-
+                <button class="js-remove">usuń</button>
                 ${task.content}
                 </li>
             `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
 
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
         });
-
-        render();
-    }
+    };
 
     const onFormSubmit = (event) => {
         event.preventDefault();
