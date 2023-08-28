522477f5d1606598a80c988e06131b2bdc8f909a
Tymek Pruszkowski <tymoteuszpruszkowski0@gmail.com>
2023-08-28 18:48:54 +0200

    toggle task done

--- js/script.js
+++ js/script.js
@@ -23,6 +23,11 @@
         render();
     }
 
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
+
     const render = () => {
         let htmlString = '';
 
@@ -31,6 +36,7 @@
                 <li
                 ${task.done ? " style=\"text-decoration: line-through\"" : ""}
                 >
+                <button class="js-done">zrobione?</button>
                 <button class="js-remove">usuń</button>
                 ${task.content}
                 </li>
@@ -44,6 +50,14 @@
         removeButtons.forEach((removeButton, index) => {
             removeButton.addEventListener("click", () => {
                 removeTask(index);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
             });
         });
     };
